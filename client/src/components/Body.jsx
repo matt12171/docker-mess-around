8ceb85b5f65ecc487a7ee10a7efaa0d361358df5
matt12171 <100318692+matt12171@users.noreply.github.com>
2024-12-04 16:49:52 +0000

    Add basic styling

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -4,10 +4,14 @@ import Map from "./Map";
 
 export default function Body() {
   return (
-    <div>
-      <Filters />
-      <Map />
-      <Charts />
+    <div id="body-main">
+      <section id="body-left">
+        <Filters />
+      </section>
+      <section id="body-right">
+        <Map />
+        <Charts />
+      </section>
     </div>
   );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div id="header">
+      <h1>Wind Farm Dashboard</h1>
     </div>
   );
 }
