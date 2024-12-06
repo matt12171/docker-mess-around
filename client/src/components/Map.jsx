f158f93be643cd99c7ee189d9d0c444de2d3c436
matt12171 <100318692+matt12171@users.noreply.github.com>
2024-12-06 14:50:09 +0000

    Add working leaflet map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,7 +1,19 @@
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+
 export default function Map() {
   return (
     <div>
-      <h1>Map</h1>
+      <MapContainer id="map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{height: "60vh", width: "100%"}}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
