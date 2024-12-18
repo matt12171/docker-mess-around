a332d3d19fb644822f160bf5bde47f82d02d682d
Matt Patterson <100318692+matt12171@users.noreply.github.com>
2024-12-18 19:36:40 +0000

    Add site name

--- client/src/data.js
+++ client/src/data.js
@@ -4,6 +4,7 @@ export const MOCK_TURBINE = [
   {
     id: 1,
     name: "Mock Turbine 1",
+    site: 'A serious wind site',
     capacity: 8, // in MW
     location: "Location 1",
     coords: [51.598221, 1.79009],
@@ -14,6 +15,7 @@ export const MOCK_TURBINE = [
   {
     id: 2,
     name: "Mock Turbine 2",
+    site: 'A serious wind site',
     capacity: 6, // in MW
     location: "Location 2",
     coords: [51.481402, 1.600944],
@@ -24,6 +26,7 @@ export const MOCK_TURBINE = [
   {
     id: 3,
     name: "Mock Turbine 3",
+    site: 'A serious wind site',
     capacity: 9, // in MW
     location: "Location 3",
     coords: [51.407841, 1.881034],
@@ -34,6 +37,7 @@ export const MOCK_TURBINE = [
   {
     id: 4,
     name: "Mock Turbine 4",
+    site: 'A serious wind site',
     capacity: 7, // in MW
     location: "Location 4",
     coords: [51.381386, 1.9],
@@ -44,6 +48,7 @@ export const MOCK_TURBINE = [
   {
     id: 5,
     name: "Mock Turbine 5",
+    site: 'A serious wind site',
     capacity: 5, // in MW
     location: "Location 5",
     coords: [51.381386, 2],
@@ -54,6 +59,7 @@ export const MOCK_TURBINE = [
   {
     id: 6,
     name: "Mock Turbine 6",
+    site: 'A serious wind site',
     capacity: 4, // in MW
     location: "Location 6",
     coords: [51.381386, 2.1],
@@ -64,6 +70,7 @@ export const MOCK_TURBINE = [
   {
     id: 7,
     name: "Mock Turbine 7",
+    site: 'A serious wind site',
     capacity: 3, // in MW
     location: "Location 7",
     coords: [51.381386, 2.2],
@@ -74,6 +81,7 @@ export const MOCK_TURBINE = [
   {
     id: 8,
     name: "Mock Turbine 8",
+    site: 'A serious wind site',
     capacity: 2, // in MW
     location: "Location 8",
     coords: [51.381386, 2.3],
@@ -84,6 +92,7 @@ export const MOCK_TURBINE = [
   {
     id: 9,
     name: "Mock Turbine 9",
+    site: 'A serious wind site',
     capacity: 1, // in MW
     location: "Location 9",
     coords: [51.381386, 2.4],
